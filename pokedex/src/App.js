b92944510f83dd7e369e952fbaafef566b513077
liviazeviani <liviapavinizeviani@gmail.com>
2021-01-18 00:06:24 -0300

    pokedex finalizada

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,26 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
-import NavBar from "./components/layout/NavBar";
-import Painel from "./components/layout/Painel";
 
- export default class App extends Component { 
-  
-  render () { 
-   
-  return (
-<div className="App">
- <NavBar />
- <div classname="container">
- <Painel />
+import backgroundImage from './pattern.png';
 
- </div>
-  
-</div>
-);
-  } 
+
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
+import SearchBar from './components/search/SearchBar';
+import Pokemon from './components/pokemon/Pokemon';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" style={{ background: `url(${backgroundImage})` }}>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
+
+export default App;
 
 
---
+++ pokedex/src/components/layout/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactLoading from 'react-loading';
+
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h5>Procurando Pokemon...</h5>
+      </React.Fragment>
+    );
+  }
+}
